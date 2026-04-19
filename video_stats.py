38b45a0b85fdc7de97d7fa2c2efacb1c40282698
axbouf <axbouf@gmail.com>
2026-04-19 12:25:34 +1000

    file is stored into local datalake

--- video_stats.py
+++ video_stats.py
@@ -3,6 +3,7 @@ import os
 from dotenv import load_dotenv
 import json 
 from pprint import pprint
+from datetime import datetime as date
 
 load_dotenv()
 channel_handle = "bbacalhau"
@@ -194,10 +195,14 @@ def extract_video_data(video_ids):
 
     except requests.exceptions.RequestException as e:
         return e
-
+def save_to_json(extracted_data):
+    file_path = f"./data/YT_data_{date.today()}.json"
+    with open(file_path, "w", encoding="utf-8") as json_spitfile:
+        json.dump(extracted_data, json_spitfile, indent=4, ensure_ascii=False)
 if __name__ == "__main__":
     # print(f"here is the playlist ID: {get_channel_playlist_id(channel_handle)}")
     playlist_id = get_channel_playlist_id()
     video_ids = get_video_ids(playlist_id)
-    extract_video_data(video_ids)
+    video_data = extract_video_data(video_ids)
+    save_to_json(video_data)
     
